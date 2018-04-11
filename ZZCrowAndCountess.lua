1bfaf6e439dd176efa219dfc98896e3bee81f39a
Zig Zichterman <github@ziggr.com>
2018-04-11 12:41:15 -0700

    Dynamic tags working
    Data might still need some touch-ups, but code and most data
    all work well now.

--- ZZCrowAndCountess.lua
+++ ZZCrowAndCountess.lua
@@ -44,6 +44,38 @@ function ZZCrowAndCountess.TooltipInterceptInstall()
     end
 end
 
+local TAG_WANTED = {
+  ["Artwork"                ] = {  nil      ,  nil              }
+, ["Cosmetics"              ] = { "Tributes", "Windhelm"        }
+, ["Devices"                ] = {  nil      ,  nil              }
+, ["Dishes and Cookware"    ] = {  nil      , "Riften"          }
+, ["Dolls"                  ] = {  nil      , "Davon's Watch"   }
+, ["Drinkware"              ] = { "Respect" , "Riften"          }
+, ["Dry Goods"              ] = {  nil      ,  nil              }
+, ["Fishing Supplies"       ] = {  nil      ,  nil              }
+, ["Furnishings"            ] = {  nil      ,  nil              }
+, ["Games"                  ] = { "Leisure" , "Davon's Watch"   }
+, ["Grooming Items"         ] = { "Tributes",  ni               }
+, ["Lights"                 ] = {  nil      ,  nil              }
+, ["Linens"                 ] = {  nil      , "Windhelm"        }
+, ["Magic Curiosities"      ] = {  nil      ,  nil              }
+, ["Maps"                   ] = {  nil      , "Stormhold"       }
+, ["Medical Supplies"       ] = {  nil      ,  nil              }
+, ["Musical Instruments"    ] = {  nil      ,  nil              }
+, ["Oddities"               ] = {  nil      , "Mournhold"       }
+, ["Ritual Objects"         ] = {  nil      , "Mournhold"       }
+, ["Scrivener Supplies"     ] = {  nil      , "Stormhold"       }
+, ["Smithing Equipment"     ] = {  nil      ,  nil              }
+, ["Statues"                ] = {  nil      , "Davon's Watch"   }
+, ["Tools"                  ] = {  nil      ,  nil              }
+, ["Toys"                   ] = { "Leisure" , nil               }
+, ["Trifles and Ornaments"  ] = {  nil      ,  nil              }
+, ["Utensils"               ] = { "Respect" , "Riften"          }
+, ["Wall Décor"             ] = {  nil      ,  nil              }
+, ["Wardrobe Accessories"   ] = {  nil      , "Windhelm"        }
+, ["Writings"               ] = {  nil      , "Stormhold"       }
+}
+
 function ZZCrowAndCountess.TooltipInsertOurText(control, item_link)
     -- Only fire for master writs.
     local item_type, specialized_item_type = GetItemLinkItemType(item_link)
@@ -54,14 +86,31 @@ function ZZCrowAndCountess.TooltipInsertOurText(control, item_link)
 
                         -- Find treasure type
     local tag_ct = GetItemLinkNumItemTags(item_link)
+    local crow_list = {}
+    local countess_list = {}
+
     for i = 1,tag_ct do
         local tag_desc, tag_category = GetItemLinkItemTagInfo(item_link,i)
         if TAG_CATEGORY_TREASURE_TYPE == tag_category then
             d(tag_desc)
+            local cc = TAG_WANTED[tag_desc]
+            if not cc then
+                d("Unknown tag: "..tostring(tag_desc))
+            else
+                if cc[1] then table.insert(crow_list, cc[1]) end
+                if cc[2] then table.insert(countess_list, cc[2]) end
+            end
         end
     end
-
-    control:AddLine("Hiya!")
+    if 0 < #crow_list then
+        control:AddLine("Crow: "..table.concat(crow_list, ", "))
+    end
+    if 0 < #countess_list then
+        control:AddLine("Countess: "..table.concat(countess_list, ", "))
+    end
+    if 0 == #crow_list and 0 == #countess_list then
+        control:AddLine("|c999999not needed for crow or countess|r")
+    end
 end
 
 
